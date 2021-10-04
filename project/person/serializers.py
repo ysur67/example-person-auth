73109098e26f9834bb7634ca1eda242994cf27c9
ysur67 <rus.muratow@gmail.com>
2021-10-04 21:45:14 +0300

    feat: update user's password

--- project/person/serializers.py
+++ project/person/serializers.py
@@ -3,10 +3,25 @@ from person.models import User
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = ("name", "birth_date",
-                  "phone", "email", "username", )
+                  "phone", "email",
+                  "username", "password", )
+
+    def get_password(self, instance):
+        password = User.objects.make_random_password()
+        return password
+
+    def validate(self, attrs):
+        if not attrs.get("phone", None) and not attrs.get("email", None):
+            raise serializers.ValidationError("The value of phone and/or email is required")
+        return super().validate(attrs)
 
     def create(self, validated_data):
-        return User.objects.create(**validated_data)
+        instance = User.objects.create(**validated_data)
+        instance.set_password(self.data["password"])
+        instance.save()
+        return instance
