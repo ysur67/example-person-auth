55f52822b2dce9ceec7d2008c831d90496496000
ysur67 <rus.muratow@gmail.com>
2021-10-05 00:29:39 +0300

    feat: convert reponse to result model

---
+++ project/person/utils/mappers.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+from rest_framework import status, response
+from person.models import RequestResult, RequestStatus
+
+
+class Mapper(ABC):
+    def __init__(self, incoming_data) -> None:
+        self._initial = incoming_data
+
+    @abstractmethod    
+    def convert(self):
+        pass
+
+
+class ResponseToResultMapper(Mapper):
+    def convert(self):
+        response_: response.Response = self._initial
+        result = RequestResult() 
+        if response_.status_code != status.HTTP_201_CREATED:
+            result.status = RequestStatus.FAILURE
+            result.text = self._get_response_messsage(response_)
+        result.save()
+        return result
+
+    def _get_response_messsage(self, response_: response.Response):
+        result = ""
+        for value in response_.data.values():
+            if isinstance(value, (list, set, )):
+                for msg in value:
+                    result += str(msg) + "\n"
+            else:
+                result += str(value) + "\n"
+        return result
--- project/person/views.py
+++ project/person/views.py
@@ -1,6 +1,13 @@
-from rest_framework import response, viewsets, status
+from rest_framework import viewsets
 from person.serializers import UserCreateSerializer
+from person.utils import Mapper, ResponseToResultMapper
 
 
 class PersonCreateView(viewsets.generics.CreateAPIView):
     serializer_class = UserCreateSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        response = super().dispatch(request, *args, **kwargs)
+        mapper: Mapper = ResponseToResultMapper(response)
+        mapper.convert()
+        return response
