fc568ff90c3f8d800fc2ae5cd0a461c66180644a
ysur67 <rus.muratow@gmail.com>
2021-10-05 10:55:12 +0300

    fix: update urls

--- project/person/urls.py
+++ project/person/urls.py
@@ -4,9 +4,11 @@ from person.views import RequestResultListView, LoginTemplateView
 
 app_name = "person"
 urlpatterns = [
-    path("api/v1/useradd/", PersonCreateView.as_view(), name="person-add"),
-    path("api/v1/download-txt/", TxtDownloadFileView.as_view(), name="txt-download"),
-    path("api/v1/download-xlsx/", XlsxDownloadFileView.as_view(), name="xslx-download"),
+    path("api/v1/", include([
+        path("useradd/", PersonCreateView.as_view(), name="person-add"),
+        path("download-txt/", TxtDownloadFileView.as_view(), name="txt-download"),
+        path("download-xlsx/", XlsxDownloadFileView.as_view(), name="xslx-download"),
+    ])),
     path("login/", LoginTemplateView.as_view(), name="person-login"),
     path("persons/", RequestResultListView.as_view(), name="request-result-list"),
 ]
