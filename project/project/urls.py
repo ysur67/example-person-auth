1efa6bef1f7e43c9f599bd1285dd8f525e3b60e4
ysur67 <rus.muratow@gmail.com>
2021-10-05 11:23:59 +0300

    feat: add swagger

--- project/project/urls.py
+++ project/project/urls.py
@@ -2,10 +2,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Тестовое задание",
+      default_version='v1',
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', include(("person.urls", "person"))),
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
